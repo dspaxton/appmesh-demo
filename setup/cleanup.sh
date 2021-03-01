d846c06f54422ef8f67035b8f28ce202f2bd0235
Darren Paxton <paxtod@amazon.co.uk>
2021-03-01 22:35:22 +0000

    Fixed cleanup to delete correct route

--- setup/cleanup.sh
+++ setup/cleanup.sh
@@ -4,7 +4,7 @@ source cfvars
 echo "Cleaning up resources"
 
 echo "Tearing down the ECS resources"
-aws --region $REGION appmesh delete-route --route-name user-agent --mesh-name my-mesh --virtual-router-name frontend-virtual-router_my-apps
+aws --region $REGION appmesh delete-route --route-name frontend-route --mesh-name my-mesh --virtual-router-name frontend-virtual-router_my-apps
 aws --region $REGION cloudformation delete-stack --stack-name $CLUSTERNAME-ecs-fargate
 aws --region $REGION cloudformation wait stack-delete-complete --stack-name $CLUSTERNAME-ecs-fargate
 
